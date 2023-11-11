f998a187b9d1d09e8492dfbf2099cd82cd9e38e2
X. Chen <xchen-me@users.noreply.github.com>
2023-11-11 14:04:56 -0800

    default vim-tmux config (no chord in vim)

--- lua/macs/plugins/vim-tmux-navigator.lua
+++ lua/macs/plugins/vim-tmux-navigator.lua
@@ -2,11 +2,12 @@ return {
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
   priority = 500, -- make sure to load this early, after the colorscheme
   config = function()
-    vim.cmd([[let g:tmux_navigator_no_mappings = 1]])
-    vim.keymap.set("n", "<C-a><C-h>", "<cmd><C-U>TmuxNavigateLeft<cr>")
-    vim.keymap.set("n", "<C-a><C-l>", "<cmd><C-U>TmuxNavigateRight<cr>")
-    vim.keymap.set("n", "<C-a><C-j>", "<cmd><C-U>TmuxNavigateDown<cr>")
-    vim.keymap.set("n", "<C-a><C-k>", "<cmd><C-U>TmuxNavigateUp<cr>")
-    vim.keymap.set("n", "<C-a>p", "<cmd><C-U>TmuxNavigatePrevious<cr>")
+    -- vim does not support key chord
+    -- vim.cmd([[let g:tmux_navigator_no_mappings = 1]])
+    -- vim.keymap.set("n", "<C-a><C-h>", "<cmd><C-U>TmuxNavigateLeft<cr>")
+    -- vim.keymap.set("n", "<C-a><C-l>", "<cmd><C-U>TmuxNavigateRight<cr>")
+    -- vim.keymap.set("n", "<C-a><C-j>", "<cmd><C-U>TmuxNavigateDown<cr>")
+    -- vim.keymap.set("n", "<C-a><C-k>", "<cmd><C-U>TmuxNavigateUp<cr>")
+    -- vim.keymap.set("n", "<C-a>p", "<cmd><C-U>TmuxNavigatePrevious<cr>")
   end,
 }
