6bf6834d9c88d5f0838c7a2d2125a60e693d05d4
X. Chen <xchen-me@users.noreply.github.com>
2023-11-12 00:53:57 -0800

    add neogit setup

--- lua/macs/plugins/neogit.lua
+++ lua/macs/plugins/neogit.lua
@@ -6,5 +6,14 @@ return {
     "sindrets/diffview.nvim", -- optional
     -- "ibhagwan/fzf-lua",              -- optional
   },
-  config = true,
+  config = function()
+    require("neogit").setup()
+    vim.keymap.set(
+      "n",
+      "<leader>gg",
+      "<cmd>Neogit pwd=$:p:h<CR>",
+      { desc = "Show the status of the current Git repository" }
+    )
+    vim.keymap.set("n", "<leader>gc", "<cmd>Neogit commit<CR>", { desc = "Create a Git commit" })
+  end,
 }
